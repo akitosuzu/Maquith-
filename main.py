d57daf6510a29ed20990a314badac1f5b73f4e46
Akito Suzuki <88513410+akitosuzu@users.noreply.github.com>
2021-08-31 03:39:07 +0000

    probably the last version :ive put some decoration :rounds played :points :how it ends

--- main.py
+++ main.py
@@ -30,19 +30,20 @@ def question_creator():
 
 def yes_no(question):
   valid = False
+  global yes_no_answer
   while not valid:
-    global yes_no_answer
     response = input(question).lower()
     if response == "yes" or response == "y":
       response = "yes"
-      yes_no_answer = "yes"
-      return " "
+      yes_no_answer = 1
+      return response
     elif response == "no" or response == "n":
       response = "no"
-      yes_no_answer = "no"
-      return " "
+      yes_no_answer = 0
+      return response
     else:
       print("Please answer yes / no")
+
 
 def statement_generator(statement, decoration):
   sides = decoration * 3
@@ -55,19 +56,22 @@ def statement_generator(statement, decoration):
 
   return " "
 
-
-
+def end_of_game():
+  statement_generator("Final Points: {}".format(points), "◌")
+  statement_generator("rounds played: {}".format(rounds_played), "◌")
+  
 #Variable - User experience
-statement_generator("welcome to MaQUIth", "●")
+statement_generator("WELCOME TO MAQUIZ", "●")
 print(" ")
 time.sleep(1)
-userexperience = input(yes_no("Have you played MaQuith before ya dog? (y/n): "))
-if yes_no_answer == "no":
+userexperience = yes_no("Have you played MaQuith before ya dog? (y/n):")
+if yes_no_answer == 0:
+  print("well then, here's instructions")
   print(" ")
-  input("instruction")
-  input("instruction")
-  print(" ")
-      
+  input("- 1: pick a level from 1 - 3, each level has different difficulties. -")
+  input("- level 1: addition of 0 - 20 / level 2: addition of 25 - 45. / level 3: addition of 50 - 100. -")
+  input("- 2: and just solve the questions!! -")
+  input("- whenever you want to quit type '000' -")
 #lists
 difficulty_levels = ["1","2","3"]
 
@@ -85,9 +89,9 @@ while True:
   try:
     difficulty_levels = int(input("What level of difficulty would you like to play? 1/2/3: "))
   except ValueError:
-    print("put in a real level ya dog")
+    statement_generator("NUMBER ONLY PLEASE","!")
   except TypeError:
-    print("level between 1 and 3 ya dog")
+    statement_generator("NUMBER ONLY PLEASE","!")
   else:
     break
 
@@ -106,22 +110,22 @@ while True:
     question_creator()
     user_answer = int(input("what is " + str(first_number) + " + " + str(second_number) + "? " ))
     if user_answer == 000:
-      statement_generator("FINAL POINTS {}".format(points), "◌")
+      end_of_game()
       break
     if user_answer == first_number + second_number:
       print(" ")
-      statement_generator("well done! its right", "R")
+      statement_generator("RIGHT ANSWER", "✔")
       print(" ")
-      statement_generator("point added +1", "-")
+      statement_generator("POINT +1", "-")
       rounds_played += 1 
       correct_answers += 1
       points += 1 
     elif user_answer != first_number + second_number:
-      statement_generator("Too bad, its wrong", "W")
-      statement_generator("point not added +0", "-")
+      statement_generator("WRONG ANSWER", "X")
+      statement_generator("POINT ADDED +0", "-")
       rounds_played += 1 
       wrong_answers += 1
   except ValueError:
-    print("only numbers ya dog") 
+    statement_generator("NUMBER ONLY PLEASE","!") 
     
 
